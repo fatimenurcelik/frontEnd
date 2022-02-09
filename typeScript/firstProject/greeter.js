f4c13e177c17d7541631e295a800771d3d9d6224
Fatime Nur <fatimenurc@gmail.com>
2022-02-09 15:38:15 +0300

    typescripte giriş

---
+++ typeScript/firstProject/greeter.js
@@ -0,0 +1,5 @@
+function greeter(name) {
+    console.log("hello " + name);
+}
+var user = "sadık";
+greeter(user);
---
+++ typeScript/firstProject/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(name){
+    console.log("hello " + name);
+}
+
+let user = "sadık";
+
+greeter(user);
