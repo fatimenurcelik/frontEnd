0feaf9221374b8adffbca05e2e2aefa3278a07e1
Fatime Nur <fatimenurc@gmail.com>
2022-05-11 15:10:02 +0300

    componentlerde degisken render edildi

---
+++ React/components/src/App.js
@@ -0,0 +1,14 @@
+
+const name = "Fatime Nur"
+const surname = "Çelik"
+
+function App() {
+  return (
+  <>
+    <h2>{`Benim adım ${name}, soyadım ${surname} `}</h2>
+  </>
+
+  );
+}
+
+export default App;
