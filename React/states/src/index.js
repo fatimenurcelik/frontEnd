674bff737403e760700f328ed3e8a6ec05074782
Fatime Nur <fatimenurc@gmail.com>
2022-05-12 14:39:21 +0300

    sayaç uygulaması

--- React/states/src/index.js
+++ React/states/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Counter from './Components/Counter.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>
 );
 
